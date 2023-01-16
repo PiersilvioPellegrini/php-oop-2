840179cca140d2f0be5cc20324ab2de8f473cc2c
PiersilvioPellegrini <pier.pellegrini.23@gmail.com>
2023-01-16 13:47:30 +0100

    Created Project

---
+++ index.php
@@ -0,0 +1,73 @@
+
+<?php
+require_once "./classes/Products.php";
+require_once "./classes/Foods.php.php";
+require_once "./classes/Toys.php";
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="imgs/favicon.ico" type="image/x-icon">
+  <link rel="icon" href="imgs/favicon.ico" type="image/x-icon">
+
+  <title>Php-oop-2| Index</title>
+
+  <!-- Third party libraries -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+  <!-- Custom css -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <main class="container pb-5">
+    <h1 class="my-5">Php-oop-2</h1>
+    <!-- your code -->
+  </main>
+
+  <script src="js/main.js"></script>
+</body>
+
+</html>
